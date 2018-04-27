26be7220f8211a245c869665bdc775a6003ac0ee
ding <playdingnow@gmail.com>
2018-04-27 16:29:37 +0800

    feat(from-batch-to-flow): case for async iterator

--- scripts/from-batch-to-flow-benchmark.js
+++ scripts/from-batch-to-flow-benchmark.js
@@ -39,16 +39,38 @@ const runFlow = async (defer) => {
     });
 };
 
+const runAsyncIterator = async (defer) => {
+  const getPostsIterator = async function*() {
+    const cursor = User.find().cursor();
+    while (true) {
+      const user = await cursor.next();
+      if (!user) return;
+      const posts = await Post.find({ username: user.name });
+      yield posts;
+    }
+  };
+  const postsNested = [];
+  for await (const post of getPostsIterator()) {
+    postsNested.push(post);
+  }
+
+  const posts = [].concat.apply([], postsNested);
+  assert.ok(posts.length === 250);
+  defer.resolve();
+};
+
 (async () => {
   await warnup();
   await drop();
   await initData();
 
   suite
-    .add('warm up', warnup, { defer: true })
+    .add('warm up 1', warnup, { defer: true })
     .add('batch', runBatch, { defer: true })
-    .add('warm up again', warnup, { defer: true })
+    .add('warm up 2', warnup, { defer: true })
     .add('flow', runFlow, { defer: true })
+    .add('warm up 3', warnup, { defer: true })
+    .add('async iterator', runAsyncIterator, { defer: true })
     .on('cycle', (event) => {
       console.log(String(event.target));
     })
